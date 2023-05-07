43d6e8dcc62079b26dc034d9af732959ce881546
NabilCopilot <nabil@cifpzonzamas.es>
2023-05-07 19:14:21 +0100

    fixing things

---
+++ public/build/assets/aside_bar-d6114149.js
@@ -0,0 +1,1 @@
+const e=document.querySelector("body"),o=e.querySelector(".sidebar"),t=e.querySelector(".toggle");t.addEventListener("click",()=>{o.classList.toggle("close")});
--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,13 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/css/aside_bar.css',
+                'resources/js/aside_bar.js',
                 'resources/css/crud.css',
+                'resources/js/crud.js',
+                'resources/css/nav_bar.css',
+                'resources/js/nav_bar.js',
+                'resources/css/playlistTable.css',
             ],
             refresh: true,
         }),
